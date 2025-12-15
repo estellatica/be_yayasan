ba69d7a144e9575cd05310d46610ae9734e2448e
abeeel <nabilaismiyatii@gmail.com>
2025-12-15 21:20:17 +0700

    db

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
-import mysql from "mysql2/promise";
+// db.js
+const mysql = require('mysql2');
 
-const db = await mysql.createConnection({
+const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT || 3307,
 });
 
 try {
